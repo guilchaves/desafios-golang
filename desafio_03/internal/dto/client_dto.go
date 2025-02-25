bae736e7b4686d81bddfa649883cab6ddad3439e
guilherme chaves <chaves.guilherme@proton.me>
2025-02-25 15:50:43 -0300

    fix: corrige dto

--- desafio_03/internal/dto/client_dto.go
+++ desafio_03/internal/dto/client_dto.go
@@ -2,12 +2,21 @@ package dto
 
 import "time"
 
-type ClientInputDTO struct {
-	Name      string    `json:"name"`
-	Cpf       string    `json:"cpf"`
-	Income    float64   `json:"income"`
-	BirthDate time.Time `json:"birthDate"`
-	Children  uint      `json:"children"`
+type CreateClientInputDTO struct {
+	Name      string  `json:"name"`
+	Cpf       string  `json:"cpf"`
+	Income    float64 `json:"income"`
+	BirthDate string  `json:"birthDate"`
+	Children  uint    `json:"children"`
+}
+
+type UpdateClientInputDTO struct {
+	ID        *int       `json:"id"`
+	Name      *string    `json:"name"`
+	Cpf       *string    `json:"cpf"`
+	Income    *float64   `json:"income"`
+	BirthDate *time.Time `json:"birthDate"`
+	Children  *uint      `json:"children"`
 }
 
 type ClientOutputDTO struct {
